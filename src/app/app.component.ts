41006093d1702a909bae1407190b078a23af72b1
Harisha V S <harisha86@yahoo.co.uk>
2022-02-25 13:02:41 +0530

    Hero Deployement - Code Ready
    Additional files are ready for deployment.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Harisha\'s first Angular 13';
+  title = 'Harisha\'s first Angular 13 Deployment';
 }
